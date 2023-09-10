9583bb460bbc09e3f9e6b0c82522f3bd6092ef3b
tainv2002 <taivannho5a@gmail.com>
2023-09-10 17:46:10 +0700

    Quickstart with swagger, run swagger UI

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,16 @@ import { UPLOAD_VIDEO_DIR } from './constants/dir'
 import cors from 'cors'
 import { createServer } from 'http'
 import initSocket from './utils/socket'
+import YAML from 'yaml'
+import fs from 'fs'
+import path from 'path'
+import swaggerUi from 'swagger-ui-express'
 
 // import '~/utils/fake'
 // import '~/utils/s3'
+
+const file = fs.readFileSync(path.resolve('twitter-swagger.yaml'), 'utf8')
+const swaggerDocument = YAML.parse(file)
 
 config()
 databaseService
@@ -36,6 +43,8 @@ app.use(cors())
 app.use(express.json())
 
 routes(app)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use('/static/video', express.static(UPLOAD_VIDEO_DIR))
 
